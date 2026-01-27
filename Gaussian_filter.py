d8b492f2b0768c8169c80163e2582ae3236c5fb3
padmanabhan4112 <psowmian@purdue.edu>
2026-01-26 19:58:32 -0500

    Update Gaussian_filter.py

--- Gaussian_filter.py
+++ Gaussian_filter.py
@@ -1,14 +1,54 @@
-#Gaussian filter for smoothing the image
-import cv2 as cv
+"""
+Code Explanation:
 
-src = cv.imread("C:\\Users\\spadm\\OneDrive\\Desktop\\test.jpg")
+2D Convolution - Averaging -
 
-img = cv.GaussianBlur(src,(5,5),10)
+As in one-dimensional signals, images also can be filtered with various low-pass filters(LPF), high-pass filters(HPF) etc. 
+LPF helps in removing noises, blurring the images etc. HPF filters helps in finding edges in the images.
 
-cv.imshow('Source image before Gaussian Filter', src)
-cv.imshow('Source image after Gaussian Filter', img)
-cv.waitKey(0)
+Behind the code, the function creates a 5x5 matrix where every element is 1/25. This ensures the sum of all elements in the kernel is 1, 
+maintaining the image brightness.
 
+This performs a 2D Convolution. The 5x5 kernel slides over the image. 
+For every pixel, it takes the average of the 5x5 neighborhood (25 pixels) and replaces the center pixel with this average value.
+"""
 
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+ 
+img = cv2.imread('test.jpg')
+ 
+kernel = np.ones((5,5),np.float32)/25
+dst = cv2.filter2D(img,-1,kernel)
+
+# can use this function also  blur = cv2.blur(img,(5,5))
+
+print("2D Convolution ( Image Filtering )")
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(dst),plt.title('2D Convolution - Averaging')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+"""
+Weighted Mean (Gaussian-style) Kernel -
+
+A weighted mean filter is a spatial filtering technique where pixels in a neighborhood are averaged, but unlike a standard "box" blur, 
+some pixels are given more "influence" (weight) than others.
+
+In most cases, the center pixel (the one being processed) is given the highest weight, and the weights decrease as you move further away 
+toward the edges of the neighborhood.
+
+"""
+
+blur = cv2.GaussianBlur(img,(5,5),0)    #GaussianBlur function, kernel (5,5) with the best sigma calculated based on the kernel size
+print("")
+print("Gaussian Blurring")
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Gaussian Blurring')
+plt.xticks([]), plt.yticks([])
+plt.show()
 
 
